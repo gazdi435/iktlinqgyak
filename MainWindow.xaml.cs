88ec57f089a4b0108c7f564fd5bd9e900a1e2359
Zsolesz <72067999+gazdi435@users.noreply.github.com>
2023-05-18 14:21:50 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Kektura
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Szakasz> lista = new List<Szakasz>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Allomanymegnyitas("kektura.txt");
+            StreamWriter sw = new StreamWriter("utvonal.txt");
+
+            foreach (Szakasz sor in lista)
+            {
+                sw.WriteLine($"{sor.Leiras};{sor.Tavolsag};{sor.Magassag};{Convert.ToString(sor.nehezseg)}");
+            }
+
+        }
+
+
+        private void Allomanymegnyitas(string eleresiUt)
+        {
+            StreamReader sr = new StreamReader(eleresiUt);
+
+            int seged = 1;
+            string sor;
+
+            while (!sr.EndOfStream)
+            {
+                sor = sr.ReadLine();
+                Szakasz uj = new Szakasz(seged, sor.Split(';')[0], Convert.ToDouble(sor.Split(';')[1]), Convert.ToInt32(sor.Split(';')[2]), Convert.ToInt32(sor.Split(';')[3]));
+                lista.Add(uj);
+                seged++;
+            }
+            sr.Close();
+            dtTartalom.ItemsSource = lista;
+        }
+
+        private void gomb_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Szakaszok száma: {lista.Count()}");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lbKeres.Content = lista.Where(x => x.Tavolsag <= Convert.ToInt32(txtKeres.Text)).Count();
+            dtTartalom.ItemsSource = lista.Where(x => x.Tavolsag <= Convert.ToInt32(txtKeres.Text)).ToList();
+        }
+
+        private void btnatlag_Click(object sender, RoutedEventArgs e)
+        {
+            lbAtlag.Content = $"{Math.Round(lista.Where(x => x.Leiras.Contains("Pilis")).Average(x => x.Teljeseites))} perc";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Szakasz ut = lista.OrderBy(x => x.Tavolsag).First();
+            MessageBox.Show($"Helyszín: {ut.Leiras}, hossz: {ut.Tavolsag}, idő: {ut.Teljeseites}");
+        }
+    }
+}
---
+++ Szakasz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kektura
+{
+    internal class Szakasz
+    {
+        int sorzsam;
+        string leiras;
+        double tavolsag;
+        int magassag;
+        int teljeseites;
+
+        public Szakasz(int sorzsam, string leiras, double tavolsag, int magassag, int teljeseites)
+        {
+            this.sorzsam = sorzsam;
+            this.leiras = leiras;
+            this.tavolsag = tavolsag;
+            this.magassag = magassag;
+            this.teljeseites = teljeseites;
+        }
+
+        public int Sorzsam { get => sorzsam; }
+        public string Leiras { get => leiras; }
+        public double Tavolsag { get => tavolsag; }
+        public int Magassag { get => magassag; }
+        public int Teljeseites { get => teljeseites; }
+
+        public string nehezseg(Szakasz ut)
+        {
+            if (ut.tavolsag > 9)
+            {
+                return "Nehéz";
+            }
+            else if (ut.magassag < 6)
+            {
+                return "Könnyű";
+            }
+            else
+            {
+                return "Közepes";
+            }
+        }
+    }
+}
